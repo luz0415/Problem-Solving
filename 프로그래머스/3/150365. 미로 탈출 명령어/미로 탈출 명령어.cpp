31ec86503e655bf5cd8a268afd094f841a09d2fa
Donghee <39627402+luz0415@users.noreply.github.com>
2024-11-11 00:36:36 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.05 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+int dx[] = {1, 0, 0, -1};
+int dy[] = {0, -1, 1, 0};
+string cmd[] = {"d", "l", "r", "u"};
+
+string solution(int n, int m, int x, int y, int r, int c, int k) {
+    x--; y--; r--; c--;
+    
+    int dist = abs(x - r) + abs(y - c);
+    if(dist > k || (k - dist) % 2 == 1)
+    {
+        return "impossible";
+    }
+    // {{좌표}, 이동 커맨드}
+    stack<pair<pair<int, int>, string>> st;
+
+    
+    st.push({{x, y}, ""});
+    
+    int nowX = x;
+    int nowY = y;
+    string nowCmd = "";
+    
+    while(k > 0)
+    {        
+        for(int i = 0; i < 4; i++)
+        {
+            int nx = dx[i] + nowX;
+            int ny = dy[i] + nowY;
+            if(!(nx >= 0 && nx < n && ny >= 0 && ny < m)) { continue; }
+            
+            dist = abs(nx - r) + abs(ny - c);
+            if(dist >= k) { continue; }
+            
+            k--;
+            nowX = nx; nowY = ny;
+            nowCmd += cmd[i];
+            break;
+        }
+    }
+    
+    return nowCmd;
+}
