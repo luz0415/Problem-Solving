574e6f2c411c28324d487bfb37a80bdf5b769722
Donghee <39627402+luz0415@users.noreply.github.com>
2025-07-05 16:17:49 +0900

    Title: 대폭발, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250705/대폭발/big-explosion.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m, r, c;
+int grid[100][100] = {0, };
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+int main() {
+    cin >> n >> m >> r >> c;
+    r--; c--;
+    // Please write your code here.
+    grid[r][c] = 1;
+    for(int t = 1; t <= m; t++)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(grid[i][j] == 1)
+                {
+                    for(int d = 0; d < 4; d++)
+                    {
+                        int nx = i + dx[d] * t;
+                        int ny = j + dy[d] * t;
+                        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+
+                        if(grid[nx][ny] == 0) grid[nx][ny] = 2;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(grid[i][j] == 2)
+                {
+                    grid[i][j] = 1;
+                }
+            }
+        }
+    }
+    
+    int count = 0;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(grid[i][j] != 0) count++;
+        }
+    }
+
+    cout << count;
+    return 0;
+}
