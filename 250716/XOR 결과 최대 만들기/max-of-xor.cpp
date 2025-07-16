52a02c9cc7b9835502040c63a65b8f90fad9ad1b
Donghee <39627402+luz0415@users.noreply.github.com>
2025-07-16 17:00:50 +0900

    Title: XOR 결과 최대 만들기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250716/XOR 결과 최대 만들기/max-of-xor.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+int A[20];
+vector<int> nums;
+int ans;
+
+void SelectNum(int step, int now)
+{
+    if(step == n)
+    {
+        int res = nums[0];
+        for(int i = 1; i < m; i++)
+        {
+            res ^= nums[i];
+        } 
+        ans = ans < res ? res : ans;
+        return;
+    }
+
+    for(int i = now + 1; i <= n; i++)
+    {
+        nums.push_back(A[i]);
+        SelectNum(step+1, i);
+        nums.pop_back();
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    // Please write your code here.
+    SelectNum(0, -1);
+    cout << ans;
+    return 0;
+}
