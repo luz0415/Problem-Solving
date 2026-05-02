be2e82a92d3f8ab6e6c6440f7d9345915a0c9304
Donghee <39627402+luz0415@users.noreply.github.com>
2026-05-02 15:42:35 +0900

    [Passed] 수의 개수 (100ms, 0MB)

---
+++ trail5/수의 개수/number-of-integers.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int arr[100000];
+int LowerBound(int find)
+{
+    int L = 0, R = N-1;
+    int idx = N;
+
+    while (L <= R)
+    {
+        int mid = (L + R) / 2;
+
+        if (arr[mid] >= find)
+        {
+            R = mid-1;
+            if(arr[mid] == find)
+            {
+                idx = min(idx, mid);
+            }
+        }
+        else
+        {
+            L = mid+1;
+        }
+    }
+    return idx;
+}
+int UpperBound(int find)
+{
+    int L = 0, R = N-1;
+    int idx = N;
+
+    while (L <= R)
+    {
+        int mid = (L + R) / 2;
+
+        if (arr[mid] > find)
+        {
+            R = mid-1;
+            idx = min(idx, mid);
+        }
+        else
+        {
+            L = mid+1;
+        }
+    }
+    return idx;
+}
+
+int main() {
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < M; i++)
+    {
+        int x; cin >> x;
+        int lb = LowerBound(x);
+        if (lb == N) { cout << 0; }
+        else { cout << UpperBound(x) - lb; }
+
+        cout << '\n';
+    }
+    return 0;
+}
